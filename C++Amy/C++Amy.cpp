3a80a6bf8f8c8a526f8d8d94ad23fdec00b0461d
Studio <Studio@DESKTOP-LSUUEP5>
2023-06-22 19:59:56 -0500

    Add project files.

---
+++ C++Amy/C++Amy.cpp
@@ -0,0 +1,17 @@
+// C++Amy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// STD is standard
+// Cout is Character output
+// Write code to swap the value of two variables
+
+#include <iostream>
+using namespace std;
+int main(){
+    double x = 10;
+    double y = 5;
+
+    double z = (x + 10) / (3 * y); 
+    cout << "Z = " << z << std ::endl
+               << "Y is " << y;
+    return 0;
+}
+
